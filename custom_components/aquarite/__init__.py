f517a1de59c972f46abcf68c8e247543f6a5f9d1
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-01-17 21:23:15 +0100

    Update and rename __init__.py to __init__.py

--- custom_components/aquarite/__init__.py
+++ custom_components/aquarite/__init__.py
@@ -1,4 +1,4 @@
-"""The Dagen integration."""
+"""The Hayward integration."""
 from homeassistant import config_entries, core
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
@@ -7,17 +7,17 @@ from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
-from .coordinator import DagenDataCoordinator
-from .dagen import Dagen
+from .coordinator import AquariteDataCoordinator
+from .aquarite import Aquarite
 
 PLATFORMS = [BINARY_SENSOR_DOMAIN, LIGHT_DOMAIN, SENSOR_DOMAIN ]
 
 async def async_setup_entry(
     hass: core.HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
-    """Set up the Dagen component."""
-    api : Dagen = await Dagen.create( async_get_clientsession(hass), entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
-    coordinator = DagenDataCoordinator(hass, api)
+    """Set up the Hayward component."""
+    api : Aquarite = await Aquarite.create( async_get_clientsession(hass), entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
+    coordinator = AquariteDataCoordinator(hass, api)
 
     coordinator.data = await hass.async_add_executor_job(api.get_pool,entry.data["pool_id"])
 
