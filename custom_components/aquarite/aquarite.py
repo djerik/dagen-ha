07b63301a325ee2ea014d147841c9928b97cac11
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-01-29 14:02:38 +0100

    Update aquarite.py

--- custom_components/aquarite/aquarite.py
+++ custom_components/aquarite/aquarite.py
@@ -120,18 +120,20 @@ class Aquarite:
         pool_data['changes'] = [{"kind": "E", "path": ["light", "status"], "lhs": 1, "rhs": 0}]
         await self.__send_command(pool_data)
 
-    async def turn_on_relay1(self, pool_id)-> None:
+    async def turn_on_relay(self, pool_id)-> None:
         """Turn on relay"""
         pool_data = self.__get_pool_as_json(pool_id)
-        pool_data['pool']['relays']['relay1']['info']['onoff'] = 1
-        pool_data['changes'] = [{"kind": "E", "path": ["relays", "relay1", "info", "onoff"], "lhs": 0, "rhs": 1}]
+        name = self._attr_name
+        pool_data['pool']['relays'][''+name+'']['info']['onoff'] = 1
+        pool_data['changes'] = '[{"kind": "E", "path": ["relays", "' + name + '", "info", "onoff"], "lhs": 0, "rhs": 1}]'
         await self.__send_command(pool_data)
 
-    async def turn_off_relay1(self, pool_id)-> None:
+    async def turn_off_relay(self, pool_id)-> None:
         """Turn off relay"""
         pool_data = self.__get_pool_as_json(pool_id)
-        pool_data['pool']['relays']['relay1']['info']['onoff'] = 0
-        pool_data['changes'] = [{"kind": "E", "path": ["relays", "relay1", "info", "onoff"], "lhs": 1, "rhs": 0}]
+        name = self._attr_name
+        pool_data['pool']['relays'][''+name+'']['info']['onoff'] = 0
+        pool_data['changes'] = '[{"kind": "E", "path": ["relays",  "' + name + '", "info", "onoff"], "lhs": 1, "rhs": 0}]'
         await self.__send_command(pool_data)
     
     async def __send_command(self, data)-> None:
