0e525d54125f6e9b15c09c082a1777d59a439a26
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-02 10:50:04 +0100

    Update aquarite.py

--- custom_components/aquarite/aquarite.py
+++ custom_components/aquarite/aquarite.py
@@ -165,7 +165,6 @@ class Aquarite:
                 "source" : "web"}
         _LOGGER.debug(pool)
         _LOGGER.debug(data)
- """ datetime.fromtimestamp(1485714600).strftime("%A, %B %d, %Y %I:%M:%S")  """
         return data
 
 
