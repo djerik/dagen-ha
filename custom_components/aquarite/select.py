1958be3c609222b60d0cd0dda0958daa5e0091bb
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-02 14:27:48 +0100

    Update select.py

--- custom_components/aquarite/select.py
+++ custom_components/aquarite/select.py
@@ -39,7 +39,7 @@ class AquariteSelectEntity(CoordinatorEntity, SelectEntity):
 
     async def async_select_option(self, option: str):
        """Set pump mode"""
-       await self._dataservice.set_pump_mode(self._attr_name, option)
+       await self._dataservice.set_pump_mode(self._attr_name, self._allowed_values.index(option))
 
     @property
     def unique_id(self):
