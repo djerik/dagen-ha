2724e31c2d715d948c606269001b90c21188b3b9
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-02 15:19:16 +0100

    Update switch.py

--- custom_components/aquarite/switch.py
+++ custom_components/aquarite/switch.py
@@ -12,6 +12,10 @@ async def async_setup_entry(hass : HomeAssistant, entry, async_add_entities) -> 
     entities = []
 
     entities.append(AquariteSwitchEntity(hass, dataservice, "Electrolysis Cover", "hidro.cover"))
+
+    entities.append(AquariteRelayEntity(hass, dataservice, "Relay1", "relays.relay1.info.onoff"))
+    entities.append(AquariteRelayEntity(hass, dataservice, "Relay2", "relays.relay2.info.onoff"))
+    entities.append(AquariteRelayEntity(hass, dataservice, "Relay3", "relays.relay3.info.onoff"))
     
     async_add_entities(entities)
 
@@ -39,6 +43,42 @@ class AquariteSwitchEntity(CoordinatorEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs):
         """Turn the entity off."""
         await self._dataservice.turn_off_hidro_cover()
+
+    @property
+    def unique_id(self):
+        """The unique id of the sensor."""
+        return self._unique_id
+
+class AquariteRelayEntity(CoordinatorEntity, SwitchEntity):
+    """Aquarite Relay Entity."""
+
+    def __init__(self, hass : HomeAssistant, dataservice, name, value_path) -> None:
+        """Initialize a Aquarite Relay Entity."""
+        super().__init__(dataservice)
+        """ self._attr_device_info =  """
+        self._dataservice = dataservice
+        self._attr_name = name
+        self._value_path = value_path
+        self._unique_id = dataservice.get_value("id") + name
+
+    @property
+    def is_on(self):
+        """Return true if the device is on."""
+        return bool(self._dataservice.get_value(self._value_path))
+        
+    @property
+    def extra_state_attributes(self) -> dict[str, str] | None:
+        attributes = {}
+        attributes['name'] = self._dataservice.get_value("relays." + self._attr_name.lower() + ".name")
+        return attributes
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the entity on."""
+        await self._dataservice.turn_on_relay(self._attr_name)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the entity off."""
+        await self._dataservice.turn_off_relay(self._attr_name)
 
     @property
     def unique_id(self):
