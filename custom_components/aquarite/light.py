247fb00f1181a93023784b410fd3c1cd0ffc6939
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-05 09:13:42 +0100

    Update light.py

--- custom_components/aquarite/light.py
+++ custom_components/aquarite/light.py
@@ -29,7 +29,7 @@ class AquariteLightEntity(CoordinatorEntity, LightEntity):
     @property
     def device_info(self):
         """Return the device info."""
-        return DeviceInfo(
+        return (
             identifiers={
                 (DOMAIN,"Aquarite_Home")
             },
