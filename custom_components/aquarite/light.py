055cb8cca6ca03c15b4633c8f5fd96ceb3c1f7d8
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-06 10:21:30 +0100

    Update light.py

--- custom_components/aquarite/light.py
+++ custom_components/aquarite/light.py
@@ -23,7 +23,7 @@ class AquariteLightEntity(CoordinatorEntity, LightEntity):
         """Initialize a Aquarite Light Sensor Entity."""
         super().__init__(dataservice)
         self._dataservice = dataservice
-        self._attr_name = dataservice.get_value("id")[-4:] + name
+        self._attr_name = dataservice.get_value("id")[-4:] + "_" +  name
         self._value_path = value_path
         self._unique_id = dataservice.get_value("id") + name
 
